824b7d5449f2df4150862c6f8e00916d533f27d1
Соколовский Станислав <sasokolovskiy@edu.hse.ru>
2026-06-03 23:11:47 +0300

    Добавил тесты для ShoppingList

--- test_recipes.py
+++ test_recipes.py
@@ -1,6 +1,7 @@
 import pytest
 from ingredient import Ingredient
 from recipe import Recipe
+from shopping_list import ShoppingList
 
 class TestIngredient:
     def test_init(self):
@@ -85,3 +86,78 @@ class TestRecipe:
         assert len(recipe) == 1
         recipe.add_ingredient(Ingredient("Вода", 5., "л"))
         assert len(recipe) == 1
+
+class TestShoppingList:
+    def test_add_recipe(self):
+        recipe = Recipe("Water")
+        recipe.add_ingredient(Ingredient("Вода", 5., "л"))
+        shop_list = ShoppingList()
+        shop_list.add_recipe(recipe, 2)
+        ingredients = shop_list.get_list()
+        assert len(ingredients) == 1
+        ingredient = ingredients[0]
+        assert ingredient.name == "Вода"
+        assert ingredient.quantity == 10.
+        assert ingredient.unit == "л"
+
+    def test_add_recipe_exception_for_invalid_portions(self):
+        recipe = Recipe("Water")
+        recipe.add_ingredient(Ingredient("Вода", 5., "л"))
+        shop_list = ShoppingList()
+        with pytest.raises(ValueError):
+            shop_list.add_recipe(recipe, 0)
+
+    def test_remove(self):
+        first = Recipe("First")
+        first.add_ingredient(Ingredient("Вода", 5., "л"))
+        second = Recipe("Second")
+        second.add_ingredient(Ingredient("Молоко", 1., "л"))
+        shop_list = ShoppingList()
+        shop_list.add_recipe(first, 2)
+        shop_list.add_recipe(second, 1)
+        shop_list.remove_recipe("Second")
+        ingredients = shop_list.get_list()
+        assert len(ingredients) == 1
+        ingredient = ingredients[0]
+        assert ingredient.name == "Вода"
+        assert ingredient.quantity == 10.
+        assert ingredient.unit == "л"
+        shop_list.remove_recipe("?")
+
+    def test_list(self):
+        first = Recipe("First")
+        first.add_ingredient(Ingredient("Вода", 5., "л"))
+        second = Recipe("Second")
+        second.add_ingredient(Ingredient("Молоко", 1., "л"))
+        third = Recipe("Third")
+        third.add_ingredient(Ingredient("Молоко", 1., "л"))
+        shop_list = ShoppingList()
+        shop_list.add_recipe(second, 1)
+        shop_list.add_recipe(third, 1)
+        shop_list.add_recipe(first, 1)
+        ingredients = shop_list.get_list()
+        assert len(ingredients) == 2
+        item_first = ingredients[0]
+        assert item_first.name == "Вода"
+        assert item_first.quantity == 5.
+        assert item_first.unit == "л"
+        item_second_third = ingredients[1]
+        assert item_second_third.name == "Молоко"
+        assert item_second_third.quantity == 2.
+        assert item_second_third.unit == "л"
+
+    def test_add(self):
+        first = ShoppingList()
+        first.add_recipe(Recipe("First", [Ingredient("Вода", 1., 'л')]), 1)
+        second = ShoppingList()
+        second.add_recipe(Recipe("Second", [Ingredient("Вода", 1., 'л')]), 1)
+        sum_shop = first + second
+        from_first = first.get_list()
+        from_second = second.get_list()
+        from_sum = sum_shop.get_list()
+        assert len(from_first) == 1
+        assert len(from_second) == 1
+        assert len(from_sum) == 1
+        assert from_first[0].quantity == 1.
+        assert from_second[0].quantity == 1.
+        assert from_sum[0].quantity == 2.
